661a91cbed9b0702a705ee7e970499cd01865cac
malaika tariq <mtariq@my.yearupunited.org>
2026-04-14 20:02:45 -0700

    Added Exercise 1B SQL

---
+++ week-02/Ex1B_first_query.sql
@@ -0,0 +1,9 @@
+/*
+Sakila is a database used to collect information about films and customers for use by a video rental business.
+*/
+
+USE Sakila;
+SELECT *
+FROM actor; -- Retrieved [number] records
+SELECT * FROM film; -- Retrieved [number] records 
+
