f9f23ea0e8d1a090e02f27d4944e2f05b7e4be21
Malaika Tariq <mtariq@my.yearupunited.org>
2026-05-05 23:23:33 -0700

    Added Ex1B

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,29 @@
+# Lab 1:
+# Question 2 & 3:
+customer_id = 123                      # I assumed ID to be numeric, but could be a string if IDs have letters.
+customer_name = "Zara Louis"           # I assumed first and last name together, but maybe should be first_name + last_name two sperate variables.
+customer_gender = "Female"             # I assumed full word, but could use "M" or "F" or numbers instead
+customer_date_of_birth ="2000-03-02"   # I assumed a string in YYYY-MM-DD format, but a date object would allow age calculations
+drivers_license_number = "D1234567"    # I assumed it must be a string and can't be an integer since it contains letters, not just numbers
+auto_policy_number = "POL-01234567"    # I assumed it must be a string since it cintains letters and dashes 
+
+# Question 4:
+my_name = "Malaika Tariq"
+birth_city = "Islamabad"
+birth_country = "Pakistan"
+
+# Lab 2:
+
+# Question 1(a):
+# Reserved Words in Python: 
+# False, None, True, def, if, raise, del, import, return,, elif, in, try, and, else, is, while, as, except, lambda, with, assert, finally, nonlocal, yield, break, for, not, class, from, or, continue, global, pass.
+ 
+# Question 1(b):
+# 1. if - An if statement is used to make a conditional statement. If the condition is True, then some action is performed.
+# 2. def - The def function is used to define a function or a method in Python.
+# 3. import - This statement is used to import modules to the project.
+# 4. return - This keyword is used to exit a function or a method and return some value.
+# 5. for - The keyword is used to create a for loop.
+
+# Words I am not familiar with:
+# ^^assert^^ ^^class^^ ^^lambda^^ ^^nonlocal^^
