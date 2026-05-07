e1c1afa9f09f1e52038d22c5feed15cacbe8e3d8
Malaika Tariq <mtariq@my.yearupunited.org>
2026-05-06 22:08:32 -0700

    Added net worth file

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,12 @@
+# Area of a Rectangle
+
+# Birthday: Month = 12 (December), Day = 19
+
+side_a = 12
+side_b = 19
+
+area = side_a * side_b
+
+print(f"Side A is {side_a}")
+print(f"Side B is {side_b}")
+print(f"The area of the rectangle is {area}")
---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,22 @@
+# Assets
+checking_account = 5000
+saving_account = 20000
+car = 25000
+investments = 8000
+
+total_assets = checking_account + saving_account + car + investments 
+
+# Debts
+credit_card = 2000
+car_loan = 4000
+mortgage = 50000
+
+total_debts = credit_card + car_loan + mortgage
+
+# Net Worth
+net_worth = total_assets - total_debts 
+
+# Output
+print(f"Your total assets are {total_assets}")
+print(f"Your total debts are {total_debts}")
+print(f"Your net worth is {net_worth}")
