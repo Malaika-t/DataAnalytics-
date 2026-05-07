15d851504636d408e39a56ad4f928229e8d2c9d7
Malaika Tariq <mtariq@my.yearupunited.org>
2026-05-06 23:09:41 -0700

    added and updated

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,9 @@
+# Area of a circle 
+import math 
+
+diameter = 19
+radius = diameter / 2
+
+area = math.pi * radius **2
+
+print(f"The area of a circle with radius {radius} is {area:.2f}")
--- week-05/Ex2A_MathScripts/rule_of_72.py
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -1,1 +1,12 @@
+# Rule of 72
 
+# Formula: Yeard to double = 72 / interest rate 
+ 
+savings = 5000
+interest_rate = 0.08  #8%
+
+years_to_double = 72 / (interest_rate * 100)
+doubled_amount = savings * 2
+
+print("Your current savings is $" + format(savings, '.2f') + ".")
+print("At a " + format(interest_rate, '.0%') +  " interest rate, your savings account will be worth $" + format(doubled_amount, '.2f') + " in " + format(years_to_double, '.1f') +  " years")
